86664adb693f9a58c44c31020093ccca763a44b0
Faizan <faizanrana369@gmail.com>
2024-06-01 00:24:21 +0500

    roter

--- 03.Express/index.js
+++ 03.Express/index.js
@@ -1,27 +1,15 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const admin = require('./router/admin');
+const shop = require('./router/shop');  
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended:false }));
 
-app.use('/user',(req,res)=>{
-    res.send('this is dummy ');
-});
+app.use(admin);
 
-app.use('/add-product',(req,res)=>{
-    res.send('<form method="POST" action="/products"><input type="text" name="title"><button type="submit">Submit</button></form>');
-});
-
-app.use('/products',(req,res)=>{
-
-console.log(req.body);
-res.redirect('/');
-
-})
-app.use('/',(req,res,)=>{
-  res.send('hello world');
-});
+app.use(shop)
 
 
 app.listen(3001)
