c221c4aeec1727414fdcc997eb5340ec0ca35525
Faizan <faizanrana369@gmail.com>
2024-05-31 22:49:12 +0500

    middleware and app parser

--- 03.Express/index.js
+++ 03.Express/index.js
@@ -1,18 +1,27 @@
-const http = require('http');
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended:false }));
 
-
-app.get((req,res,next)=>{
-    console.log('hello world');
- next()
+app.use('/user',(req,res)=>{
+    res.send('this is dummy ');
 });
 
-app.get((req,res)=>{
-    res.send('<h1>hello world again</h1>');
+app.use('/add-product',(req,res)=>{
+    res.send('<form method="POST" action="/products"><input type="text" name="title"><button type="submit">Submit</button></form>');
 });
 
-const server = http.createServer(app);
-server.listen(3002);
+app.use('/products',(req,res)=>{
+
+console.log(req.body);
+res.redirect('/');
+
+})
+app.use('/',(req,res,)=>{
+  res.send('hello world');
+});
+
+
+app.listen(3001)
